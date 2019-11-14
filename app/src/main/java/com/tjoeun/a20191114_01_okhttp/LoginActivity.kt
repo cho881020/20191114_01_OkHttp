5dfa309b6b621a2d8406fb4e2aed03625a6a9a1d
조경진 <kj_cho@idealidea.co.kr>
2019-11-14 20:41:38 +0900

    POST - 로그인 요청 따라하기

--- app/src/main/java/com/tjoeun/a20191114_01_okhttp/LoginActivity.kt
+++ app/src/main/java/com/tjoeun/a20191114_01_okhttp/LoginActivity.kt
@@ -1,16 +1,18 @@
 package com.tjoeun.a20191114_01_okhttp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjoeun.a20191114_01_okhttp.utils.ServerUtil
 
-class MainActivity : BaseActivity() {
-
+class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         setupEvents()
         setValues()
+
+        ServerUtil.postRequestLogin(mContext, "cho881020", "qlalfqjsgh!", null)
+
     }
 
     override fun setupEvents() {
