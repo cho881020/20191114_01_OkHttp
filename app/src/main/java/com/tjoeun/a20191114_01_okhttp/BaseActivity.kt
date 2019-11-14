22af847720984123205a61fe00674e4502b80240
조경진 <kj_cho@idealidea.co.kr>
2019-11-14 19:56:05 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeun/a20191114_01_okhttp/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.tjoeun.a20191114_01_okhttp
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    var mContext: Context = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
